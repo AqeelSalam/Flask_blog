70ffd595ded68aa7b4dc97f39b909985814ae2e4
Mohammed Salam <ms@Mohammeds-MacBook-Pro.local>
2020-03-02 12:36:20 -0600

    Created html templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Mohammed Salam',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Febraury 29, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 15, 2019'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return f'<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return f'<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
